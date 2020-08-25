b4fc6dda695e11b10b2c586e3a2deece60cb19bc
Double-X <lkllun@yahoo.com.hk>
2020-08-25 22:27:06 +0800

    Create Codes Adding Delay Between Becoming Able To Act And Actually Inputting Actions.js

---
+++ Codes Adding Delay Between Becoming Able To Act And Actually Inputting Actions.js
@@ -0,0 +1,14 @@
+/*
+ * 1. Some users want some enemies and autobattle/confused actors to wait for a while instead of inputting actions right after they can do so
+ * 2. There's no way to tweak the amount of delay between becoming able to input actions and actually doing so on the user side
+ * 3. It's an excellent feature but I'm too busy for quite a while, and I don't want users to wait for too long
+ * 4. Any such tunes must be done by accessing the plugin implementation codes
+ * 5. The following implementation parts are the minimum amount of relevant codes to the users, and they won't work without the rest of the plugins
+ */
+Game_SATBPhaseTypes.prototype._fillCoreATB = function(e) {
+    this.addCoreATB(this._fillRate(e))
+}, Game_SATBPhaseTypes.prototype._onCoreATBBecomeFull = function() {
+    this._battler.canMove() && this._battler.makeActions()
+}, Game_SATBPhaseTypes.prototype._onCoreATBBecomeNotFull = function() {
+    this._battler.clearActions()
+};
